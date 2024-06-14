d857f1d8ad86007dddd04bd2943325480f68c455
meirman_is_creator <sarsenbaymeyirman@gmail.com>
2024-06-14 10:09:58 +0500

    i finished

--- server/index.js
+++ server/index.js
@@ -1,26 +1,46 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
 
-const http = require('http').Server(app)
-const cors = require('cors')
-const socketIO = require('socket.io')(http, {
-    cors:{
-        origin:'http://localhost:5173'
-    }
-})
+const http = require("http").Server(app);
+const cors = require("cors");
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
 
-app.get('api',(req, res)=>{
-    res.json({
-        message:'Hello'
-    })
-})
-socketIO.on("connection",(socket)=>{
-    console.log(`${socket.id} user connected`);
-    socket.on('disconnect', ()=>{
-        console.log(`${socket.id} disconnect`)
-    })
-})
-http.listen(PORT,()=>{
-    console.log('Server working');
-})
+app.use(cors());
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Hello",
+  });
+});
+
+const users = [];
+
+socketIO.on("connection", (socket) => {
+  console.log(`${socket.id} user connected`);
+
+  socket.on("message", (data) => {
+    socketIO.emit("response", data);
+  });
+
+  socket.on("typing", (data) => {
+    socket.broadcast.emit("responseTyping", data);
+  });
+
+  socket.on("newUser", (data) => {
+    users.push(data);
+    socketIO.emit("responseNewUser", users);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} disconnected`);
+  });
+});
+
+http.listen(PORT, () => {
+  console.log("Server working on port " + PORT);
+});
